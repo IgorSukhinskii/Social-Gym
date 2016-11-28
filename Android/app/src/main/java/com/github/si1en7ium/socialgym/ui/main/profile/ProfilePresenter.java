60dc0e7d7a0ce2b78b0b3f174f65b43a26da1850
mishiuss <ilyasovaolya@gmail.com>
2016-11-29 00:20:14 +0200

    create Profile source package

---
+++ Android/app/src/main/java/com/github/si1en7ium/socialgym/ui/main/profile/ProfilePresenter.java
@@ -0,0 +1,15 @@
+package com.github.si1en7ium.socialgym.ui.main.profile;
+
+import com.github.si1en7ium.socialgym.ui.base.BasePresenter;
+
+import javax.inject.Inject;
+
+
+class ProfilePresenter extends BasePresenter<ProfileMvpView> {
+
+    @Inject
+    ProfilePresenter() {
+    }
+
+
+}
