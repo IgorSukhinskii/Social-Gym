f1f79764d8c4c75d0e1af3cdc2227d6dc2d05441
mishiuss <ilyasovaolya@gmail.com>
2016-12-01 15:02:03 +0200

    SG-31 1.0 version of navigation drawer

--- Android/app/src/main/java/com/github/si1en7ium/socialgym/ui/main/MainActivity.java
+++ Android/app/src/main/java/com/github/si1en7ium/socialgym/ui/main/MainActivity.java
@@ -2,8 +2,10 @@ package com.github.si1en7ium.socialgym.ui.main;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -16,8 +18,13 @@ import com.github.si1en7ium.socialgym.R;
 import com.github.si1en7ium.socialgym.ui.base.BaseActivity;
 import com.github.si1en7ium.socialgym.ui.main.events.EventsFragment;
 import com.github.si1en7ium.socialgym.ui.main.profile.ProfileFragment;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
@@ -28,15 +35,25 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import timber.log.Timber;
-
 public class MainActivity extends BaseActivity implements MainMvpView {
+
+
+
 
     private static final String EXTRA_TRIGGER_SYNC_FLAG =
             "com.github.si1en7ium.socialgym.ui.main.MainActivity.EXTRA_TRIGGER_SYNC_FLAG";
-    @Inject MainPresenter mainPresenter;
-    @BindView(R.id.fab) FloatingActionButton fab;
-    @BindView(R.id.toolbar) Toolbar toolbar;
+    @Inject
+    MainPresenter mainPresenter;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
     private List<PrimaryDrawerItem> drawerItems;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     /**
      * Return an Intent to start this Activity.
@@ -68,6 +85,7 @@ public class MainActivity extends BaseActivity implements MainMvpView {
         PrimaryDrawerItem drawerProfile = new PrimaryDrawerItem()
                 .withIdentifier(1)
                 .withName(R.string.drawer_profile)
+
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
 
@@ -79,12 +97,12 @@ public class MainActivity extends BaseActivity implements MainMvpView {
                     }
                 });
 
+        DividerDrawerItem dividerDrawerItem = new DividerDrawerItem();
+
         PrimaryDrawerItem drawerEventsList = new PrimaryDrawerItem()
-                .withIdentifier(2)
+                .withIdentifier(0)
                 .withName(R.string.drawer_events_list)
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
                         switchToFragment(EventsFragment.newInstance());
                         fab.show();
@@ -94,7 +112,7 @@ public class MainActivity extends BaseActivity implements MainMvpView {
                 });
 
         PrimaryDrawerItem drawerMyEvents = new PrimaryDrawerItem()
-                .withIdentifier(3)
+                .withIdentifier(2)
                 .withName(R.string.drawer_my_events)
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
@@ -108,18 +126,25 @@ public class MainActivity extends BaseActivity implements MainMvpView {
                 });
 
         // set up navigation drawer
-        Drawer drawer = new DrawerBuilder()
+        Drawer drawerResult = new DrawerBuilder()
                 .withActivity(this)
                 .withToolbar(toolbar)
-                .addDrawerItems(drawerProfile)
                 .addDrawerItems(drawerEventsList)
+                .addDrawerItems(dividerDrawerItem)
                 .addDrawerItems(drawerMyEvents)
+                .addDrawerItems(dividerDrawerItem)
+                .addDrawerItems(drawerProfile)
+                .withHeader(R.layout.drawer_header)
                 .build();
 
-        switchToFragment(EventsFragment.newInstance());
+       // switchToFragment(EventsFragment.newInstance());
 
         mainPresenter.attachView(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -150,16 +175,25 @@ public class MainActivity extends BaseActivity implements MainMvpView {
                 .replace(R.id.fragment_placeholder, fragment)
                 .addToBackStack(null)
                 .commit();
-       setFabVisibility(fragment.isFabShown());
+        setFabVisibility(fragment.isFabShown());
     }
 
     @Override
     public void onBackPressed() {
+
+
+        //if (drawer.isDrawerOpen()) {
+        //    drawer.closeDrawer();
+        //} else {
+        //    super.onBackPressed();
+        // }
+
         super.onBackPressed();
         BaseMainFragment currentFragment = (BaseMainFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_placeholder);
         setFabVisibility(currentFragment.isFabShown());
     }
+
 
     private void setFabVisibility(boolean isFabShown) {
         if (isFabShown && !fab.isShown()) {
@@ -174,19 +208,61 @@ public class MainActivity extends BaseActivity implements MainMvpView {
         getSupportActionBar().setTitle(title);
     }
 
-    private  void selectItem (int position) {
+    private void selectItem(int position) {
         BaseMainFragment fragment = null;
 
         switch (position) {
-            case 1: fragment = new ProfileFragment();
+            case 1:
+                fragment = new ProfileFragment();
                 break;
-            case 2: fragment = new EventsFragment();
+            case 2:
+                fragment = new EventsFragment();
                 break;
-            default: break;
+            default:
+                break;
         }
-        if (fragment !=null) {
-            switchToFragment(fragment);}
-        else {Log.e("MainActivity", "Error");}}
+        if (fragment != null) {
+            switchToFragment(fragment);
+        } else {
+            Log.e("MainActivity", "Error");
+        }
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 
     //Обработчик кликов
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
@@ -195,6 +271,8 @@ public class MainActivity extends BaseActivity implements MainMvpView {
             selectItem(position);
         }
     }
+
+
 }
 
 ////////////
