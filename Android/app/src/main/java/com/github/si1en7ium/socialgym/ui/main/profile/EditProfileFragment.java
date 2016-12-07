13c90bf50b7cdde52cc1b97f2188a1fec327170c
Gleb Bogodukhov <Gleb.Bogodukhov@student.lut.fi>
2016-12-07 21:24:15 +0200

    Kind of completed the edit profile layout, although it needs some styling and date picker/gender spinner code written - NO errors now

--- Android/app/src/main/java/com/github/si1en7ium/socialgym/ui/main/profile/EditProfileFragment.java
+++ Android/app/src/main/java/com/github/si1en7ium/socialgym/ui/main/profile/EditProfileFragment.java
@@ -43,17 +43,8 @@ public class EditProfileFragment extends BaseMainFragment {
         View view = inflater.inflate(main_edit_profile_fragment, container, false);
 
         ButterKnife.bind(this, view);
-       return view;
+        return view;
     }
-    //Попыточка намутить спиннер
-    Spinner spinner = (Spinner) findViewById(R.id.typeOfSportsSelector);
-    // Create an ArrayAdapter using the string array and a default spinner layout
-    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-            R.array.genders, android.R.layout.simple_spinner_item);
-    // Specify the layout to use when the list of choices appears
-    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    // Apply the adapter to the spinner
-    spinner.setAdapter(adapter);
 
 }
 
